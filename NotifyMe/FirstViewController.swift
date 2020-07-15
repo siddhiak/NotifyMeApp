e0c85c5cc419b42985ad753728615002426b5322
siddhiak <47063373+siddhiak@users.noreply.github.com>
2020-07-15 11:50:10 -0700

    Initial Commit

---
+++ NotifyMe/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  NotifyMe
+//
+//  Created by Siddhi Kabadi on 7/15/20.
+//  Copyright © 2020 Siddhi Kabadi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NotifyMe/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  NotifyMe
+//
+//  Created by Siddhi Kabadi on 7/15/20.
+//  Copyright © 2020 Siddhi Kabadi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
