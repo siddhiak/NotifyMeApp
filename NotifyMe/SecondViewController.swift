29572232a80bdfd12695d3da5061c9e51256cbe9
siddhiak <47063373+siddhiak@users.noreply.github.com>
2020-07-15 14:32:04 -0700

    git commit

--- NotifyMe/SecondViewController.swift
+++ NotifyMe/SecondViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
